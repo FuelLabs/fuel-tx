e8d1c5757bbdaf257188467be2b3821cc2a3a253
rakita <dragan0rakita@gmail.com>
2022-01-09 18:08:42 +0100

    Added comment for unused let

--- src/receipt.rs
+++ src/receipt.rs
@@ -871,6 +871,8 @@ impl bytes::Deserializable for Receipt {
     fn from_bytes(bytes: &[u8]) -> io::Result<Self> {
         let mut instance = Self::ret(Default::default(), 0, 0, 0);
 
+        // We are sure that all needed bytes are written or error would happen.
+        // unused let is here to silence clippy warning for this check.
         let _ = instance.write(bytes)?;
 
         Ok(instance)
